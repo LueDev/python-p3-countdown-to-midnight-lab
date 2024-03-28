ab7d6c091375ff2dfd6334e5362c2a8112b66679
PapiLuee <41023873+LueDev@users.noreply.github.com>
2024-03-28 19:52:07 -0400

    All tests passing. imported the time module (built in) for the sleep timer.

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,17 @@
 # your code goes here!
+def countdown(num: int) -> str:
+    while(abs(num) > 0):
+        print(f"{num} SECOND(S)!")
+        num -= 1
+    print("HAPPY NEW YEAR!")
+
+import time
+def countdown_with_sleep(num: int) -> str:
+    while(abs(num) > 0):
+        print(f"{num} SECOND(S)!")
+        num -= 1
+        time.sleep(1)
+    print("HAPPY NEW YEAR!")
+    
+if __name__ == "__main__":
+    countdown_with_sleep(10, 1)
